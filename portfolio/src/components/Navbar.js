1cc1d50ee55f150a7a21d3617ccaa5ee21067b51
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-10 23:59:17 +0800

    using makeStyles

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box } from '@material-ui/core';
 import { ArrowBack, AssignmentInd, Home, Apps, ContactMail } from '@material-ui/icons';
 import avatar from '../assets/cartoon1.png';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+//CSS Styles
+const useStyles = makeStyles ({
+    menuSliderContainer :{
+        width:250,
+        background:"#511".
+        height: "30rem"
+    }
+})
+
+
 
 const Navbar = () => {
+    const classes = useStyles();
     return (
         <div>
-            <Box component="div">
+            <Box className={classes.menuSliderContainer} component="div">
                 <Avatar src={avatar} alt="Pradnesh Sanderan"/>
 
             </Box>
