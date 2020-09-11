5a44ccf9724ce573ac40c02c9020b3b2f760b470
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-11 23:38:32 +0800

    add react-typed and header text

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import {Typography, Avatar, Grid, Box} from '@material-ui/core';
+import { Typography, Avatar, Grid, Box } from '@material-ui/core';
 import avatar from '../assets/cartoon1.png';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
         <Box>
-            <Avatar src={avatar} alt="Pradnesh Sanderan"/>
-            
+            <Avatar src={avatar} alt="Pradnesh Sanderan" />
+            <Typography variant="h4">
+                <Typed strings={["Pradnesh Sanderan"]} 
+                typeSpeed={40} />
+            </Typography>
+            <br />
+            <Typography variant="h4">
+                <Typed strings={["Computer Science Major", "The University of Edinburgh"]} 
+                typeSpeed={40} 
+                 backSpeed={60}
+                 loop/>
+            </Typography>
+
+
         </Box>
     )
 }
