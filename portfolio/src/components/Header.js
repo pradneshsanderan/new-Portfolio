7bcc5e7a595deef1baa1cb57aecdfbb553c8c861
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-11 23:57:07 +0800

    background & pic css

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,21 +2,50 @@ import React from 'react'
 import { Typography, Avatar, Grid, Box } from '@material-ui/core';
 import avatar from '../assets/cartoon1.png';
 import Typed from 'react-typed';
+import { makeStyles } from '@material-ui/core/styles';
+
+//CSS Styles
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+        margin: theme.spacing(1),
+    },
+    title: {
+        color: "tomato",
+    },
+    subtitle: {
+        color: "tan",
+        marginBottom: "3rem"
+    },
+    typedContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100vw",
+        textAlign: "center",
+        zIndex: 1
+    }
+}))
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <Box>
-            <Avatar src={avatar} alt="Pradnesh Sanderan" />
-            <Typography variant="h4">
-                <Typed strings={["Pradnesh Sanderan"]} 
-                typeSpeed={40} />
+        <Box className={classes.typedContainer}>
+            <Grid container justify="center">
+                <Avatar src={avatar} alt="Pradnesh Sanderan" className={classes.avatar} />
+            </Grid>
+            <Typography variant="h4" className={classes.title}>
+                <Typed strings={["Pradnesh Sanderan"]}
+                    typeSpeed={40} />
             </Typography>
             <br />
-            <Typography variant="h4">
-                <Typed strings={["Computer Science Major", "The University of Edinburgh"]} 
-                typeSpeed={40} 
-                 backSpeed={60}
-                 loop/>
+            <Typography variant="h4" className={classes.subtitle}>
+                <Typed strings={["Computer Science Major", "The University of Edinburgh"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop />
             </Typography>
 
 
