1104a823f0a549e2fcf52cc01ca4cd3459c8c469
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-13 00:39:46 +0800

    timeLineItem

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -34,6 +34,65 @@ const useStyles = makeStyles(theme => ({
             }
         }
 
+    },
+    timeLineItem: {
+        padding: "1rem",
+        borderBottom: "2px solid tan",
+        position: "relative",
+        margin: "1rem 3rem 1rem 1rem",
+        clear: "both",
+        "&:after": {
+            content: "''",
+            position: "absolute"
+        },
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            right: "-0.625rem",
+            top: "calc(50%)",
+            borderStyle: "solid",
+            borderColor: "tomato tomato transparent transparent",
+            borderWidth: "0.625rem",
+            transform: "rotate(45deg)"
+        },
+        [theme.breakpoints.up("md")]: {
+            width: "44%",
+            margin: "1rem",
+            "&:nth-of-type(2n)": {
+                float: "right",
+                margin: "1rem",
+                borderColor: "tan"
+            },
+            "&:nth-of-type(2n):before": {
+                right: "auto",
+                left: "-0.625rem",
+                borderColor: "transparent transparent tomato tomato"
+            }
+        }
+    },
+    timeLineYear: {
+        textAlign: "center",
+        maxWidth: "9.375rem",
+        margin: "0 3rem 0 auto",
+        fontSize: "1.8rem",
+        background: "tomato",
+        color: "white",
+        lineHeight: 1,
+        padding: "0.5rem 0 1rem",
+        "&:before": {
+            display: "none"
+        },
+        [theme.breakpoints.up("md")]: {
+            textAlign: "center",
+            margin: "0 auto",
+            "&:nth-of-type(2n)": {
+                float: "none",
+                margin: "0 auto"
+            },
+            "&:nth-of-type(2n):before": {
+                display: "none"
+            }
+        }
     }
 }));
 
@@ -47,7 +106,7 @@ const Resume = () => {
                     Education
             </Typography>
                 <Box component="div" className={classes.timeLine}>
-                    <Typography variant="h2">
+                    <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
                         2013
                 </Typography>
 
