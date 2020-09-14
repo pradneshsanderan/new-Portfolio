9db7f6e0ec3b43fb8a913cc042d5a7bcfe18b5b8
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-14 23:57:30 +0800

    cards css

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Box, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Navbar from './Navbar';
+import project1 from '../js.jpg';
 
-
+const useStyles = makeStyles({
+    mainContainer:{
+        background: "#233",
+        height:"100%",
+    },
+    cardContainer:{
+        maxWidth:345,
+        margin:"3rem",
+        margin:"5rem auto",
+    }
+})
 
 const Portfolio = () => {
+    const classes = useStyles();
     return (
-        <Box component="div">
+        <Box component="div" className={classes.mainContainer}>
             <Navbar />
-            <Grid container justify="center" alignItems="center">
+            <Grid container justify="center" >
                 {/*Project 1 */}
                 <Grid item xs={12} sm={8} md={6}>
-                    <Card>
+                    <Card className={classes.cardContainer}>
                         <CardActionArea>
                             <CardMedia
                                 component="img"
