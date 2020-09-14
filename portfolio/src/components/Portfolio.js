281550691734915219a7c5bad454abaadd0b3755
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-15 00:32:05 +0800

    projects frame done

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -3,16 +3,17 @@ import { Box, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, B
 import { makeStyles } from '@material-ui/core/styles';
 import Navbar from './Navbar';
 import project1 from '../js.jpg';
+import project2 from '../rc.jpg';
 
 const useStyles = makeStyles({
-    mainContainer:{
+    mainContainer: {
         background: "#233",
-        height:"100%",
+        height: "100%",
     },
-    cardContainer:{
-        maxWidth:345,
-        margin:"3rem",
-        margin:"5rem auto",
+    cardContainer: {
+        maxWidth: 345,
+        margin: "3rem",
+        margin: "5rem auto",
     }
 })
 
@@ -30,7 +31,7 @@ const Portfolio = () => {
                                 component="img"
                                 alt="Project 1"
                                 height="140"
-                                image={project1}/>
+                                image={project1} />
 
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
@@ -41,15 +42,77 @@ const Portfolio = () => {
                                     something
                                 </Typography>
                             </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Code
-                                </Button>
-                                <Button size="small" color="primary">
-                                  Live Demo
-                                </Button>
-                            </CardActions>
                         </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Code
+                                </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                                </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                {/*Project 2 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project 2"
+                                height="140"
+                                image={project2} />
+
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 2
+                                </Typography>
+
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    something
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Code
+                                </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                                </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                {/*Project 3 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project 3"
+                                height="140"
+                                image={project2} />
+
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 3
+                                </Typography>
+
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    something
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Code
+                                </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                                </Button>
+                        </CardActions>
                     </Card>
                 </Grid>
 
