9b5ace44b75e6911b3e7186d61a6b6d327d8a6bb
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-15 23:51:27 +0800

    input css

--- portfolio/src/components/Contacts.js
+++ portfolio/src/components/Contacts.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
 import { TextField, Typography, Button, Grid, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import Navbar from './Navbar';
+
+
+
+const InputField = withStyles({
+    root:{
+        "& label.Mui-focused" :{
+            color:"tomato",
+
+        },
+        "& label" : {
+            color:"tan",
+        
+        },
+        "& .MuiOutlinedInput-root" :{
+            "& fieldset" :{
+                borderColor: "tan",
+            },
+            "&:hover fieldset" :{
+                borderColor: "tan",
+
+            },
+        }
+    },
+})(TextField);
+
+
+
 const Contacts = () => {
     return (
-        <div>
-            hello
-        </div>
-    )
+        <Box component="div">
+            <Grid container justify="center">
+                <Box component="form">
+                    <Typography variant="h5">
+                         Contact me
+                    </Typography>
+                    <InputField 
+                    fullWidth={true}
+                    label="Name"
+                    variant="outlined"
+                    
+                    
+                    />
+                </Box>
+            </Grid>
+        </Box>
+    );
 }
 
 export default Contacts
