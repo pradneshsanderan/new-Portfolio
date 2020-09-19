5815447659943e29372a70bf7380ee0a93271a0b
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-19 23:45:32 +0100

    footer

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction} from '@material-ui/core';
+import Facebook from '@material-ui/icons/Facebook';
+import Twitter from '@material-ui/icons/Twitter';
+import Instagram from '@material-ui/icons/Instagram';
+
+
+
+const Footer = () => {
+    return (
+        <BottomNavigation>
+            
+        </BottomNavigation>
+    )
+}
+
+
+
+export default Footer
