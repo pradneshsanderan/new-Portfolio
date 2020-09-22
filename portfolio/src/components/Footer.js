6d765f871cf4a54a600282ae3ec2ac870be17bdd
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-22 23:10:54 +0100

    added resume

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,18 +4,20 @@ import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import CodeIcon from '@material-ui/icons/Code';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import DescriptionIcon from '@material-ui/icons/Description';
+import Resume from './cv.pdf';
 
-const useStyles= makeStyles({
-    root:{
-        "& .MuiBottomNavigationAction-root":{
-            minWidth:0,
-            maxWidth:250,
+const useStyles = makeStyles({
+    root: {
+        "& .MuiBottomNavigationAction-root": {
+            minWidth: 0,
+            maxWidth: 250,
         },
-        "& .MuiSvgIcon-root":{
-            fill:"tan",
-            "&:hover" : {
+        "& .MuiSvgIcon-root": {
+            fill: "tan",
+            "&:hover": {
                 fill: "tomato",
-                fonstSize:"1.8rem"
+                fonstSize: "1.8rem"
             }
         }
     }
@@ -39,6 +41,13 @@ const Footer = () => {
                 className={classes.root}
                 style={{ padding: 0 }}
                 icon={<LinkedInIcon />} />
+
+            <BottomNavigationAction
+                className={classes.root}
+                style={{ padding: 0 }}
+                icon={<DescriptionIcon />} />
+            <a href={Resume} class="btn-rounded-white" rel="noopener noreferer" target="_blank"></a>
+
         </BottomNavigation>
     );
 }
