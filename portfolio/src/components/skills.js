00a2273fa7b4eeba6c51cad57a75dd670f57ae0c
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-10-05 23:53:46 +0100

    new commit

--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar';
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Box } from '@material-ui/core';
 
 export const skills = () => {
     return (
-        <div>
+        <>
+        <Navbar />
             
-        </div>
+        </>
     )
 }
