44cb981e644e81b38cc6b747d4375b8c7b7eeb3c
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-10-05 23:57:05 +0100

    skills background

--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -2,12 +2,23 @@ import React from 'react'
 import Navbar from './Navbar';
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Box } from '@material-ui/core';
+const useStyles = makeStyles(theme => ({
+    mainContainer: {
+        background: "#233"
+    }
+}));
 
-export const skills = () => {
+
+ const Skills = () => {
     return (
         <>
         <Navbar />
+        <Box component="header" className={classes.mainContainer}>
+
+        </Box>
             
         </>
     )
 }
+
+export default Skills;
