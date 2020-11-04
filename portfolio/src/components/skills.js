663c216a3d59f680f68d37bb7a907004a9ffd354
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-11-04 21:23:02 +0000

    skills

--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -1,69 +1,27 @@
-import React from 'react'
+import React,{Component} from 'react'
+import {Grid, Cell} from 'react-mdl';
 import Navbar from './Navbar';
-import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Box } from '@material-ui/core';
-const useStyles = makeStyles(theme => ({
-    mainContainer: {
-        background: "#233"
+
+class Skills extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar />
+                <Grid>
+                    <Cell className="resume-left-col" col={4}>
+                        <div style={{textAlign:'center'}}>
+                        <img src="" alt="avatar"
+                        style={{height:'200px'}} />
+                        </div>
+
+
+
+                    </Cell>
+                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
+                </Grid>
+            </div>
+        )
     }
-}));
-
-
-const Skills = () => {
-    const classes = useStyles();
-    return (
-        <>
-            <Navbar />
-            <Box component="header" className={classes.mainContainer}>
-                <Box>
-                    <h1>
-                        Experience
-                    </h1>
-                    <h2>
-                        Technical skills
-                    </h2>
-                    <h3>
-                        Languages
-                    </h3>
-                    <ul>
-                        Java
-                        Python
-                        C
-                        Haskell
-                        HTML/CSS
-                    </ul>
-                    <h3>Frameworks</h3>
-                    <ul>
-                        ReactJs
-                        React Native
-                        Android Studio
-
-                    </ul>
-                    <h3>others</h3>
-                    <ul>
-                        Git
-                        Adobe suite
-                        Microsoft Office Suite
-                        Lego Mindstorms Robotics
-                        
-                    </ul>
-
-                    <h2>
-                        languages
-                    </h2>
-                    <ul>
-                        English
-                        Malay
-                        Tamil
-                    </ul>
-                    <h2>
-                        
-                    </h2>
-                </Box>
-            </Box>
-
-        </>
-    )
 }
 
 export default Skills;
