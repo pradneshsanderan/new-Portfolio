da63a032b8fd5d538359b00cd0a0b972ce78449c
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-11-05 14:52:14 +0000

    education

--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import {Grid, Cell,Button} from 'react-mdl';
 import Navbar from './Navbar';
+import { Typography, Box } from '@material-ui/core';
 
 class Skills extends Component{
     render(){
@@ -8,17 +9,15 @@ class Skills extends Component{
             <div>
                 <Navbar />
                 <div style={{display:'flex',flexDirection:'row'}}>
-                    <div style={{width:'50%'}}></div>
-                    <div style={{width:'50%'}}></div>
-                </div>
-                <div style={{textAlign:'center'}}>
+                    <div style={{width:'50%'}} className="resume-left-col">
+
+                    <div style={{textAlign:'center'}}>
                     <img
                     src ='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Happy&eyebrowType=Default&mouthType=Twinkle&skinColor=Brown'
                     alt="avatar"
                     style={{height:'300px'}}
                     />
-                </div>
-                <h2 style={{paddingTop:'2em'}}> Pradnesh Sanderan</h2>
+                     <h2 style={{paddingTop:'2em'}}> Pradnesh Sanderan</h2>
                 <h4 style={{color:'grey'}}>Programmer</h4>
                 <hr style={{borderTop:'3px solid #833fb2',width: '50%'}}/>
                 <p>Self-assured computer science student and team leader, currently studying in the University of Edinburgh ,with diverse experience utilizing programming languages 
@@ -35,6 +34,24 @@ class Skills extends Component{
                 <p>pradneshsanderan@gmail.com</p>
                 
                 <hr style={{borderTop:'3px solid #833fb2',width: '50%'}}/>
+                </div>
+                    </div>
+                    <div style={{width:'50%'}} className="resume-right-col">
+                        <h1 style={{fontSize:"40px", color:"tomato",textTransform: "uppercase"}}>
+                            Education
+                        </h1>
+
+                        <h2>University</h2>
+                        <div >
+                            <div style={{width:'50%'}}><h4>The University of Edinburgh</h4></div>
+                            <div style={{width:'50%'}}><h4>2019-2023</h4></div>
+                        </div>
+                        <h4>BSc Computer Science</h4>
+                        <h5>Courses Taken</h5>
+                    </div>
+                </div>
+                
+               
                
         </div>
         )
