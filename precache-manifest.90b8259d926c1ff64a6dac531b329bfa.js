8d780033cc0d43da21657e867f2ea081a76bc272
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-11-11 21:52:52 +0000

    Updates

---
+++ precache-manifest.90b8259d926c1ff64a6dac531b329bfa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfcaa78dba54b355dd14ba243a92878",
+    "url": "/pradnesh/index.html"
+  },
+  {
+    "revision": "f57c90e7d841b093f5a7",
+    "url": "/pradnesh/static/css/2.f76e3c0a.chunk.css"
+  },
+  {
+    "revision": "24eae2ac1bf93a0fffa1",
+    "url": "/pradnesh/static/css/main.7a315a80.chunk.css"
+  },
+  {
+    "revision": "f57c90e7d841b093f5a7",
+    "url": "/pradnesh/static/js/2.c52d68b2.chunk.js"
+  },
+  {
+    "revision": "4233e42ae1d14a4f0f23cb066c59a9a3",
+    "url": "/pradnesh/static/js/2.c52d68b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24eae2ac1bf93a0fffa1",
+    "url": "/pradnesh/static/js/main.45c32c38.chunk.js"
+  },
+  {
+    "revision": "79857e3adedabae9a721",
+    "url": "/pradnesh/static/js/runtime-main.afde9e59.js"
+  },
+  {
+    "revision": "72702e3aae5f4a889e6ecc0e0866d856",
+    "url": "/pradnesh/static/media/cartoon1.72702e3a.png"
+  },
+  {
+    "revision": "00e5e42b86dec7cebe02427bcac45b32",
+    "url": "/pradnesh/static/media/cv.00e5e42b.pdf"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/pradnesh/static/media/js.54fd4234.jpg"
+  },
+  {
+    "revision": "0602a72d2d617f75dbcd1dfceace3c74",
+    "url": "/pradnesh/static/media/lp.0602a72d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/pradnesh/static/media/rc.a8b012a0.jpg"
+  }
+]);
